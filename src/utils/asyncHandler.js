62e6e92ccddae5ee5ba5ef1b91ebc9da8c881bdb
Ritika19rani <ritika2006rani@gmail.com>
2025-07-15 14:22:59 +0530

    adding new files

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res.next)).catch((error)=>next(error))
+    }
+}
+
+
+export{asyncHandler}
+
+//PPRO14207639158
+
+// const asyncHandler=(fn)=>async()=>{
+//     try {
+//         await fn(req,res,next) 
+//     } catch (error) {
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
+
