128ede7c231927f819b26579557921a77b978718
Ritika19rani <ritika2006rani@gmail.com>
2025-07-23 17:10:40 +0530

    change in controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,22 +1,22 @@
-const asyncHandler=(requestHandler)=>{
-    return (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res.next)).catch((error)=>next(error))
-    }
-}
+// const asyncHandler=(requestHandler)=>{
+//     return (req,res,next)=>{
+//         Promise.resolve(requestHandler(req,res.next)).catch((error)=>next(error))
+//     }
+// }
 
 
 export{asyncHandler}
 
 //PPRO14207639158
 
-// const asyncHandler=(fn)=>async()=>{
-//     try {
-//         await fn(req,res,next) 
-//     } catch (error) {
-//         res.status(error.code||500).json({
-//             success:false,
-//             message:error.message
-//         })
-//     }
-// }
+const asyncHandler=(fn)=>async(req,res,next)=>{
+    try {
+        await fn(req,res,next) 
+    } catch (error) {
+        res.status(error.code||500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
 
